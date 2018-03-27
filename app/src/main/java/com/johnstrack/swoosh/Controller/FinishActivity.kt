e8ff2d4e26670568f7f0139ee0ef406c2e9b8d4b
John Strack <Braveheart22@GMail.com>
2018-03-27 17:12:23 -0400

    Keep instance state when rotating

--- app/src/main/java/com/johnstrack/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/johnstrack/swoosh/Controller/FinishActivity.kt
@@ -2,20 +2,21 @@ package com.johnstrack.swoosh.Controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.johnstrack.swoosh.Model.Player
 import com.johnstrack.swoosh.R
-import com.johnstrack.swoosh.Utilities.EXTRA_LEAGUE
-import com.johnstrack.swoosh.Utilities.EXTRA_SKILL
+import com.johnstrack.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        val league = intent.getStringExtra(EXTRA_LEAGUE)
-        val skill = intent.getStringExtra(EXTRA_SKILL)
+        val player = intent.getParcelableExtra<Player>(EXTRA_PLAYER)
 
-        searchLeaguesText.text = "Looking for $league $skill league near you... "
+        searchLeaguesText.text = "Looking for a ${player.league} ${player.skill} league near you... "
     }
 }
--- app/src/main/java/com/johnstrack/swoosh/Utilities/Constants.kt
+++ app/src/main/java/com/johnstrack/swoosh/Utilities/Constants.kt
@@ -4,5 +4,4 @@ package com.johnstrack.swoosh.Utilities
  * Created by John on 3/27/2018 at 1:44 PM.
  */
 
-const val EXTRA_LEAGUE = "league"
-const val EXTRA_SKILL = "skill"
+const val EXTRA_PLAYER = "player"
